10655cd82a49ec3f30c627f30d62aaa25c339788
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-11-15 20:21:12 +0000

    Setup tauri app

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,22 @@
+// Tauri App Configuration
+
+// Modules
+mod filesystem;
+
+// Components
+use filesystem::{ scan_path, save_file_as };
+
+
+// Configuration
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![
+            scan_path,
+            save_file_as,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
