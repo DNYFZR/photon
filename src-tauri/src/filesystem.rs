ba83c91713ffd66e6b49013bad8fc0e1f3868f43
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-11-15 20:21:37 +0000

    Added filesystem module

---
+++ src-tauri/src/filesystem.rs
@@ -0,0 +1,89 @@
+// Filesystem Operations
+use std::fs;
+use std::io::{BufRead, Write};
+
+#[tauri::command]
+pub fn scan_path(path:&str) -> Vec<String> {
+    // return a vector containing the contents of a directory or file
+    let path_meta = fs::metadata(path);
+    
+    match path_meta {
+        Ok(path_meta) => {
+            if path_meta.is_dir() {
+                return list_dir(path);
+            }
+
+            return read_file(path);
+        },
+        Err(e) => vec![String::from("error"), e.to_string()],
+        
+    }   
+}
+
+fn read_file(path:&str) -> Vec<String> {
+    let file = fs::read(path);
+    match file {
+        Ok(res) => { 
+            return res.lines().map(|x| x.unwrap()).collect::<Vec<String>>();
+        },
+        Err(e) => {
+            return vec![String::from(format!("File not found : {}", e)),]
+        },
+    }
+}
+
+fn list_dir(path:&str) -> Vec<String> {
+    // list files & sub-directories for path 
+    let entries = fs::read_dir(path);
+
+    match entries {
+        Ok(res) => {
+            let files = res.filter_map(|entry| {
+                let path = entry.ok()?.path();
+                if path.is_file() || path.is_dir() {
+                    path.file_name()?.to_str().map(|s| s.to_owned())
+                } else {
+                    None
+                }
+            }).collect();
+        
+            return files;
+        },
+        Err(e) => {return vec![String::from("error"), e.to_string(), ];},
+    }
+}
+
+
+#[tauri::command]
+pub fn save_file_as(path:&str, data:&str) -> String  {
+    // Save data to file on path, if path is available & not currently occupied 
+    let check_exists = fs::exists(path);
+
+    match check_exists {
+        // File exists
+        Ok(true) => return String::from(format!("Cannot save as, file already exists on path : {}", path)),
+        
+        // File doesn't exist & path accessible
+        Ok(false) => {
+            return save_file(path, data);
+        },
+
+        // Path inaccessible
+        Err(e) => return String::from(format!("Could not verify status of path : {}", e)),
+    }
+}
+
+fn save_file(path:&str, data:&str) -> String {
+    // Create new file 
+    match fs::File::create_new(path) {
+        // Try to write bytes data
+        Ok(mut new_file) => {
+            match new_file.write_all(data.as_bytes()) {
+                Ok(_) => return String::from(format!("File saved to path : {}", path)),
+                // Handle errors
+                Err(e) => return String::from(format!("Error saving to path : {}", e)),
+             }
+        },
+        Err(e) => return String::from(format!("Error creating file : {}", e)), 
+    }
+}
