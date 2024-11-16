596ee61e4757a8f6db6f0e0f4192e2f7bbcccec6
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-11-16 16:15:21 +0000

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,29 @@
 // Tauri API
 import { invoke } from "@tauri-apps/api/core";
 
-// test
-import CodeEditor from '@uiw/react-textarea-code-editor';
-import rehypePrism from 'rehype-prism-plus';
-
 // React UI
 import "./App.css";
-import logo from "/wave-128.png";
-
+import noteIcon from "/icons/notes-128.png";
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Sidebar from "./components/Sidebar";
 
+// Code UI
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import rehypePrism from 'rehype-prism-plus';
+
+
 function App() {
   // App controls
   const [formAction, setFormAction] = useState<string>("");
-  
+  const [fontSize, setFontSize] = useState<number>(15);
+
   // Content controls
-  const [activeDir, setActiveDir] = useState<string>("");
-  const [activeDirContent, setActiveDirContent] = useState<string[]>([]);
-  
+  const [cwd, setCwd] = useState<string>("");
+  const [currentDir, setCurrentDir] = useState<string>("");
+    
   const [activePath, setActivePath] = useState<string>("");
-  const [activeContent, setActiveContent] = useState<string[]>([]);
+  const [_, setActiveContent] = useState<string[]>([]);
   const [activeContentString, setActiveContentString] = useState<string>("");
   const [contentType, setContentType] = useState<string>("");
   
@@ -35,6 +36,9 @@ function App() {
     return `${parts[0]}\n`.concat(...parts.slice(1).map((v) => `${v}\n`));
   }
 
+  async function getCWD() {
+    setCwd(await invoke("get_cwd"));
+  }
   async function scanPath(path:string) {
     setActivePath(path);
 
@@ -43,20 +47,28 @@ function App() {
     setActiveContentString(requestedContent.length > 0 ? toString(requestedContent) : "")
   }
 
-  async function saveAs(path:string, data:string) {
-    setSaveResult(await invoke("save_file_as", {path: path, data: data}));
+  async function saveToFile(method:string, path:string, data:string) {
+    if(method === "create"){
+      setSaveResult(await invoke("save_file_as", {path: path, data: data}));
+    }
+
+    if(method === "overwrite"){
+      setSaveResult(await invoke("save_file", {path: path, data: data}));
+    }
+
   }
 
   async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if(formAction === "setDir" && currentDir.length > 0){
+      await invoke("set_cwd", {path: currentDir});
+      setCurrentDir("");
+      getCWD();
+    }
 
     if(formAction === "OK"){
       if (activePath){
         scanPath(activePath);
-      }
-      
-      else if(activeDir){
-        setActiveDirContent(await invoke("scan_path", { path: activeDir }));
       }
       
       else {
@@ -66,7 +78,11 @@ function App() {
         setSaveResult([]);
       }
     } else if(formAction === "SAVE" && savePath && activeContentString.length > 0){
-      saveAs(savePath, activeContentString);
+      saveToFile("create", savePath, activeContentString);
+      setSavePath("");
+
+    } else if(formAction === "OVERWRITE" && savePath && activeContentString.length > 0){
+      saveToFile("overwrite", savePath, activeContentString);
       setSavePath("");
 
     } else if (formAction === "SAVE" && !savePath && activeContentString.length > 0) {
@@ -78,69 +94,85 @@ function App() {
     setActiveContentString(text);
     setActiveContent(text.split("\n"));
   }
-
+  
   return (
-    <main className="container">
+    <main className="container" onLoad={getCWD}>
       {/* Header */}
-      <div className="row">
-        <img src={logo} className="logo-photon" alt="Photon logo" />
-        <h3>Photon</h3>
-      </div>
-
+      <h2 className="app-header">Photon</h2>
+      
       {/* Sidebar */}
       <Sidebar>
-        <form className="row" onSubmit={(e) => handleFormSubmit(e)}>
+        {/* Set CWD */}
+        <form className="row" onSubmit={(e) => handleFormSubmit(e)}>  
+          <button type="submit" onClick={() => setFormAction("setDir")}>Set</button>
+          <input 
+            placeholder="Set directory..."
+            value={currentDir}
+            onChange={(e) => setCurrentDir(e.target.value)}
+          />
+        </form> 
+
+        {/* Filesystem Scan */}
+        <form className="col" onSubmit={(e) => handleFormSubmit(e)}>
           <div className="row">
+            {/* User filepath */}
+            <div className="col">
+              <button type="submit" onClick={() => setFormAction("OK")}>Load</button>
+              <button type="submit" onClick={() => setFormAction("OVERWRITE")}>Save</button>
+            </div>  
+
             <input
-              placeholder="Attach directory..."
-              value={activeDir}
+              placeholder="Source path..."
+              value={activePath}
               onChange={(e) => {
-                setActiveDir(e.target.value);
+                setActivePath(e.target.value);
+                setActiveContent([]);
+                setActiveContentString("");
               }}
             />
-            <button type="submit" onClick={() => setFormAction("OK")}>Load</button>        
           </div>
-        </form>
-        
-        <div className="app-menu-content">
-          {activeDirContent.length > 0 && <pre>{toString(activeDirContent)}</pre>}
-        </div>
+          
+          {/* User save as location */}
+          <div className="row">
+            <button type="submit" onClick={() => setFormAction("SAVE")}>Save As</button>
+            <input
+              placeholder="Output path..."
+              value={savePath}
+              onChange={(e) => setSavePath(e.target.value)}
+            />                
+          </div>
+
+          <pre className="highlight-text">{saveResult.length > 0 ? saveResult : null}</pre>
+        </form>    
       </Sidebar>
       
-      {/* Filesystem */}
-      <form className="row" onSubmit={(e) => handleFormSubmit(e)}>
+      {/* Main user interface */}
+      <div className="row">
         <div className="row">
-          <input
-            placeholder="Source path..."
-            value={activePath}
-            onChange={(e) => {
-              setActivePath(e.target.value);
-              setActiveContent([]);
-              setActiveContentString("");
-            }}
+          <pre>Font Size : </pre>
+          <input 
+            type="number"
+            min={10}
+            step={1}
+            max={40}
+            value={fontSize} 
+            onChange={(e) => setFontSize(Number(e.target.value))}
           />
+        </div>
 
+        <div className="row">
+          <pre>File format : </pre>
           <input
             className="input-small"
-            placeholder="Ext."
+            placeholder="Format..."
             value={contentType}
             onChange={(e) => setContentType(e.target.value)}
           />
-          <button type="submit" onClick={() => setFormAction("OK")}>Load</button>        
         </div>
 
-        <div className="row">
-          <input
-            placeholder="Output path..."
-            value={savePath}
-            onChange={(e) => setSavePath(e.target.value)}
-          />        
-          <button type="submit" onClick={() => setFormAction("SAVE")}>Save</button>
-        </div>
-      </form> 
+      </div>
 
-      <pre>{saveResult.length > 0 && saveResult}</pre>
-
+      
       <div className="app-code-editor">
         <CodeEditor
           value={activeContentString}
@@ -151,8 +183,15 @@ function App() {
             [rehypePrism, { ignoreMissing: true, showLineNumbers: true }],
           ]}
           className="app-file-render"
+          style={{fontSize: fontSize}}
         />
       </div>
+
+      <div className="app-bottom-bar">
+        <img src={noteIcon} className="app-bottom-bar-icon" />
+        <pre className="highlight-text"><b>{cwd}\{activePath && activePath !== "."? activePath : null}</b></pre>
+      </div>
+
     </main>
   );
 }
