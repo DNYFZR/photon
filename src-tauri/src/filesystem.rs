01a59872b2abe2c3bf05cabdf299f62ec646b6fe
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-11-18 18:52:49 +0000

    Added system nav

--- src-tauri/src/filesystem.rs
+++ src-tauri/src/filesystem.rs
@@ -2,6 +2,9 @@
 use std::fs;
 use std::io::{BufRead, Write};
 use std::env;
+use std::path::Path;
+
+// System Navigation
 
 #[tauri::command]
 pub fn scan_path(path:&str) -> Vec<String> {
@@ -62,6 +65,37 @@ fn list_dir(path:&str) -> Vec<String> {
     }
 }
 
+#[tauri::command]
+pub fn get_cwd() -> String {
+    let dir = env::current_dir();
+
+    match dir {
+        Ok(dir) => {
+            if let Some(res) = dir.to_str() {
+                return String::from(res);
+            } else {
+                return String::from("Error getting current directory");
+            }
+        },
+        Err(e) => return e.to_string(),
+    }
+}
+
+#[tauri::command]
+pub fn set_cwd(path:&str) -> String {
+    // Format path
+    let path_formatted = path.replace("%USERPROFILE%", &env::var("USERPROFILE").unwrap());
+    
+    // Set CWD
+    let dir = env::set_current_dir(Path::new(&path_formatted));
+
+    match dir {
+        Ok(_) => return String::from(format!("Directory updated : {}", path_formatted)),
+        Err(e) => return e.to_string(),
+    }
+}
+
+// File Saving
 
 #[tauri::command]
 pub fn save_file_as(path:&str, data:&str) -> String  {
@@ -84,8 +118,8 @@ pub fn save_file_as(path:&str, data:&str) -> String  {
 
 #[tauri::command]
 pub fn save_file(path:&str, data:&str) -> String {
-    // Create new file 
-    match fs::File::create_new(path) {
+    // Create new file
+    match fs::File::create(path) {
         // Try to write bytes data
         Ok(mut new_file) => {
             match new_file.write_all(data.as_bytes()) {
@@ -98,28 +132,3 @@ pub fn save_file(path:&str, data:&str) -> String {
     }
 }
 
-#[tauri::command]
-pub fn get_cwd() -> String {
-    let dir = env::current_dir();
-
-    match dir {
-        Ok(dir) => {
-            if let Some(res) = dir.to_str() {
-                return String::from(res);
-            } else {
-                return String::from("Error getting current directory");
-            }
-        },
-        Err(e) => return e.to_string(),
-    }
-}
-
-#[tauri::command]
-pub fn set_cwd(path:&str) -> String {
-    let dir = env::set_current_dir(path);
-
-    match dir {
-        Ok(_) => return String::from(format!("Directory updated : {}", path)),
-        Err(e) => return e.to_string(),
-    }
-}
